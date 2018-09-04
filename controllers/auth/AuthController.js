b4564ab3a493caf07a3908505ee77455620a57e6
daudalvologic <daud@alvologic.com>
2018-09-04 15:40:10 +0530

    Add Joi and Lodash and update AuthController

--- controllers/auth/AuthController.js
+++ controllers/auth/AuthController.js
@@ -1,50 +1,70 @@
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const Joi = require('joi');
 
 
 module.exports = {
     register: function(req, res){
-        let hashedPassword = bcrypt.hashSync(req.body.password, 8);
+        let hashedPassword = bcrypt.hashSync(req.body.password, 8); 
+        //Synchonized so no need to then
 
         //Validae with JOI
+        
+        const valSchema = {
+            name: Joi.string().min(4).max(200).required(),
+            email: Joi.string().min(4).max(200).email().required(),
+            password: Joi.string().min(4).max(200).required(),
+        }
+        
+        let valResult = Joi.validate(req.body,valSchema);
+
+        if(valResult.error) return res.status(400).send(valResult);
+
+        
 
         User.findOne({
             where:{
                 email: req.body.email
             }
         }).then(function(user){
-            if(user){
-                return res.status(400).send('User already registerd.');
-            }
-        });
+            // if(user){
+            //     return res.status(400).send('User already registerd.');
+            // }
 
-        
+            //ABOVE line will not stop next line exe
 
-        User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        }).then(function(user){
+            if(user) return res.status(400).send('User already registerd.');
+
+            User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: hashedPassword
+            }).then(function(user){
+                
+                var token = jwt.sign({
+                    id: user._id
+                }, process.env.JWT_SECRET, {
+                    expiresIn: 86400 // expires in 24 hours
+                });
+                return res.status(200).send({
+                    auth: true,
+                    token: token
+                });
+    
+            }).catch(function(err){
+                return res.status(500).json({
+                    error:err
+                });
+            });
             
-            var token = jwt.sign({
-                id: user._id
-            }, process.env.JWT_SECRET, {
-                expiresIn: 86400 // expires in 24 hours
-            });
-            return res.status(200).send({
-                auth: true,
-                token: token
-            });
-
-        }).catch(function(err){
-            return res.status(500).json({
-                error:err
-            });
         });
+
+       
 
     },
     getToken: function(req,res){
         res.status(200).send('ok');
     }
+    
 }
 
